71da2d8cdb05bd838b8f7c82d3e04ba13ebee3d4
meenyweeny <kmeye0419@gmail.com>
2021-11-04 15:23:11 +0900

    initial commit #v1

---
+++ 12181642_KyoungminLee.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int t; //질의 수
+char cmd;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin>>t;
+    while(t--){
+        cin>>cmd;
+        if(cmd=='I'){ //애플리케이션 등록 기능 수행
+        }
+        else if(cmd=='F'){ //애플리케이션 검색 기능 수행
+        }
+        else if(cmd=='R'){ //애플리케이션 업데이트 기능 수행
+        }
+        else if(cmd=='D'){ //애플리케이션 할인 기능 수행
+        }
+        
+    }
+}
+
