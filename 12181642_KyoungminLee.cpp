263b265a1cc5f934f41dad71753e1f54b8434f1b
meenyweeny <kmeye0419@gmail.com>
2021-11-04 15:35:43 +0900

    main 뼈대 #v2

--- 12181642_KyoungminLee.cpp
+++ 12181642_KyoungminLee.cpp
@@ -14,12 +14,44 @@ int main(){
     while(t--){
         cin>>cmd;
         if(cmd=='I'){ //애플리케이션 등록 기능 수행
+            //출력 형식 -> tree에서 애플리케이션 정보가 저장된 노드의 깊이
+            
+            //tree에 애플리케이션 등록하고 저장된 깊이 출력
+            //만약 이미 있는 application? -> 등록 거절하고 이미 있는 것의 깊이 출력
+            int id;
+            string appName;
+            int volume, price;
+            cin>>id;
+            cin>>appName;
+            cin>>volume>>price;
         }
         else if(cmd=='F'){ //애플리케이션 검색 기능 수행
+            //애플리케이션 존재하면? 트리에서의 깊이, 애플리케이션 이름, 용량, 가격 출력
+            //애플리케이션 존재하지 않으면? NULL 출력
+            
+            int id;
+            cin>>id;
+            
         }
         else if(cmd=='R'){ //애플리케이션 업데이트 기능 수행
+            //애플리케이션 존재하면? 정보를 업데이트하고, 그 노드의 깊이 출력
+            //존재하지 않으면? NULL 출력
+            
+            int id;
+            string updateName;
+            int updateVolume, updatePrice;
+            
+            cin>>id;
+            cin>>updateName;
+            cin>>updateVolume>>updatePrice;
+            
         }
         else if(cmd=='D'){ //애플리케이션 할인 기능 수행
+            //범위 rangeFront <= id <= rangeBack 내의 id를 가진 애플리케이션을 모두 탐색 후 가격에 p%의 할인율 적용
+            int rangeFront,rangeBack;
+            double salePercent;
+            cin>>rangeFront>>rangeBack;
+            cin>>salePercent;
         }
         
     }
